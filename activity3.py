9fa4dd15b2a1cb26d21afa91377222f46aa3ad1f
nathanielorbe332-stack <nathanielorbe332@gmail.com>
2026-09-07 22:23:14 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,38 @@
+# variables
+# integer
+years_old = 100
+print("Maria is", years_old, "years old")
+tall_cm = 170
+print("Maria stands", tall_cm, "cm tall")
+cash = 100
+print("Maria is carrying", cash, "dollars")
+buddies = 5
+print("Maria goes with", buddies, "teammates")
+# float
+avg_grade = 6.9
+print("Maria scored a", avg_grade, "rating at the arcade with Batman")
+fries_cost = 25.5
+print("Maria spent", fries_cost, "dollars for pizza")
+body_weight = 700.5
+print("Maria weighs a whoping t", body_weight, "kg")
+temp_c = 36.5
+print("Maria's temp is reading", temp_c, "degrees")
+# string
+spot = "Pizza Hut"
+print("Maria is suddenly craving", spot, "in New York")
+hero = "Batman"
+print("Maria bumped into", hero)
+meal = "Burger"
+print("Maria is chowing down on a", meal)
+destination = "Gotham City"
+print("Maria is headed to", destination)
+# boolean
+is_online = True
+is_offline = False
+print("Batman asks if Maria likes eating burgers upside down. Maria replied", is_offline)
+is_hungry = True
+print("Is Maria hungry:", is_hungry)
+is_sleepy = False
+print("Is Maria sleepy:", is_sleepy)
+is_happy = True
+print("Is Maria feeling happy:", is_happy)
